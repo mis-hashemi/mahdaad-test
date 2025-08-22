d4833140fa7e12fe2100d5c2d143fc7f62590b7b
Fatemeh <fatemeh.developer@gmail.com>
2025-08-22 12:06:07 +0330

    chore(challenge1): wire up SMS service in entrypoint

--- cmd/challenge1/main.go
+++ cmd/challenge1/main.go
@@ -1,4 +1,38 @@
 package main
 
+import (
+	"context"
+	"fmt"
+	"github.com/mis-hashemi/mahdaad-test/adapter/sms_client/test_client"
+	"github.com/mis-hashemi/mahdaad-test/internal/sms_service"
+	"github.com/mis-hashemi/mahdaad-test/pkg/circuitbreaker"
+	"github.com/mis-hashemi/mahdaad-test/pkg/retry"
+	"time"
+)
+
 func main() {
+	cb := circuitbreaker.NewCircuitBreaker(
+		circuitbreaker.WithName("SMS CB"),
+		circuitbreaker.WithFailureThreshold(3),
+		circuitbreaker.WithTimeout(3*time.Second),
+	)
+
+	r := retry.New(
+		retry.WithExponentialBackoff(10*time.Millisecond, 50*time.Millisecond),
+	)
+
+	client, _ := test_client.New()
+
+	handler := sms_service.New(client, cb, r)
+
+	ctx := context.Background()
+	phone := "+989135718862"
+	msg := "Hello from demo!"
+
+	err := handler.SendSMS(ctx, phone, msg)
+	if err != nil {
+		fmt.Println("SendSMS error:", err)
+	}
+
+	fmt.Println("CircuitBreaker state:", cb.State())
 }
