72edd029e500ac620bf5ae114e3f2eaa1f7f429b
Fatemeh <fatemeh.developer@gmail.com>
2025-08-22 12:30:03 +0330

    chore(challenge4): add main.go for demo - Initialize services and queue - Demonstrate profile sync workflow

--- cmd/challenge4/main.go
+++ cmd/challenge4/main.go
@@ -1,4 +1,28 @@
 package main
 
+import (
+	"context"
+	"time"
+
+	"github.com/mis-hashemi/mahdaad-test/internal/services/profile_sync"
+	"github.com/mis-hashemi/mahdaad-test/internal/services/user_service"
+	"github.com/mis-hashemi/mahdaad-test/pkg/queue"
+)
+
 func main() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	q := queue.New(10)
+
+	// start consumer for syncing profile
+	q.StartConsumer(ctx, profile_sync.Handler)
+
+	// simulate user service
+	uService := user_service.New(q)
+	uService.UpdateProfile("u1", "new email")
+	uService.UpdateProfile("u2", "new phone")
+
+	// wait a bit to see retries/success
+	time.Sleep(10 * time.Second)
 }
